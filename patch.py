3825493e9fc9bd57b48322dda55e040f2e5cfc36
DAmbrose-hub <55997679+DAmbrose-hub@users.noreply.github.com>
2022-03-01 11:11:38 +0530

    Create patch.py

---
+++ patch.py
@@ -0,0 +1,9 @@
+Page up
+import subprocess,getpass,os
+import json,sys
+hostname = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+print("Username " + username)
+print("Password "+ password)
+print(hostname+ " is patched")
